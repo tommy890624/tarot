3e037e3cd5ac001b2a8d0c410aa5c034ae8d7e30
Tommy <tommy890624@gmail.com>
2024-09-05 18:19:45 +0800

    Initial Commit

---
+++ tarot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tarot
+//
+//  Created by 黃鈺展 on 2024/9/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
